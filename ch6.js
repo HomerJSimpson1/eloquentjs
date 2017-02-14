10b529ea7253ee5f747b53338bbae279c6e9ca9e
Peterson <petersj@nih.gov>
2017-02-14 17:52:29 -0500

    Latest commit

--- ch6.js
+++ ch6.js
@@ -66,6 +66,95 @@ var blackRabbit = new Rabbit("black");
 console.log(blackRabbit.type);    // black
 
 
+Rabbit.prototype.speak = function(line) {
+    console.log("The " + this.type + " rabbit says '" + line + "'");
+};
+
+blackRabbit.speak("Doom ...");         // The black rabbit says 'Doom ...'
+
+
+
+// Overriding Derived Properties
+
+Rabbit.prototype.teeth = "small";
+console.log(killerRabbit.teeth);          // small
+killerRabbit.teeth = "long, sharp, and bloody";
+console.log(killerRabbit.teeth);          // long, sharp, and bloody
+console.log(blackRabbit.teeth);           // small
+console.log(Rabbit.prototype.teeth);      // small
+
+
+console.log(Array.prototype.toString == Object.prototype.toString) // false
+
+console.log([1, 2].toString());     // 1,2
+console.log(Object.prototype.toString.call([1, 2]));  // [object Array]
+
+
+
+// PROTOTYPE INTERFERENCE
+
+/*
+A prototype can be used at any time to add new properties and methods
+to all objects based on it. For example, it might become necessary for
+our rabbits to dance.
+*/
+
+Rabbit.prototype.dance = function() {
+    console.log("The " + this.type + " rabbit dances a jig.");
+};
+
+killerRabbit.dance();      //The killer rabbit dances a jig.
+
+
+var map = {};
+function storePhi(event, phi) {
+    map[event] = phi;
+}
+
+storePhi("pizza", 0.069);
+storePhi("touched tree", -0.081);
+
+Object.prototype.nonsense = "hi";    // adds an enumerable property "nonsense"
+for (var name in map) {
+    console.log(name);
+}
+// pizza
+// touched tree
+// nonsense
+
+console.log("nonsense" in map);  // true
+console.log("toString" in map);  // true
+
+// Delete the problematic property
+delete Object.prototype.nonsense;
+
+// Adds a nonenumerable property "hiddenNonsense"
+Object.defineProperty(Object.prototype, "hiddenNonsense", {enumerable: false, value: "hi"});
+
+for (var name in map) {
+    console.log(name);
+}
+
+// pizza
+// touched tree
+
+console.log(map.hiddenNonsense);
+// hi
+
+// However, this will still show up using the "in" operator.
+// If instead we use hasOwnProperty, then derived properties will not be listed.
+
+console.log(map.hasOwnProperty("toString"));    // false
+
+// So, for the future, usually better to use "hasOwnProperty" rather than "in", e.g.
+for (var name in map) {
+    if (map.hasOwnProperty(name)) {
+	// ... this is an own property
+    }
+}
+
+
+// PROTOTYPE-LESS OBJECTS
 
 
 
