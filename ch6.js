ee7aef4d0b31494035e4e4a6b4cc2666fb78813e
HomerJSimpson1 <vuke93@gmx.com>
2017-02-15 23:34:54 -0500

    Latest commit late evening

--- ch6.js
+++ ch6.js
@@ -223,3 +223,28 @@ console.log(colWidths(rows));
 */
 
 
+function drawTable(rows) {
+    var heights = rowHeights(rows);
+    var widths = colWidths(rows);
+
+    function drawLine(blocks, lineNo) {
+	return blocks.map(function(blcok) {
+	    return block[lineNo];
+	}).join(" ");
+    }
+
+    function drawRow(row, rowNum) {
+	var blocks = row.map(function(cell, colNum) {
+	    return cell.draw(widths[colNum], heights[rowNum]);
+	});
+	return blocks[0].map(function(_, lineNo) {
+	    return drawLine(blocks, lineNo);
+	}).join("\n");
+    }
+
+    return rows.map(drawRow).join("\n");
+}
+
+
+
+
