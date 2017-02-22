acab379e06b448937362b1286f06d0add0b7a436
JP <vuke93@gmx.com>
2017-02-21 20:06:27 -0500

    End chapter 6, start chapter 7

--- ch6.js
+++ ch6.js
@@ -463,7 +463,7 @@ function StretchCell(inner, width, height) {
 
 StretchCell.prototype.minWidth = function() {
     if (this.inner.minWidth() >= this.width) {
-	console.log(this.inner.minWidth());
+	//console.log(this.inner.minWidth());
 	return this.inner.minWidth();
     }
     else
@@ -510,7 +510,268 @@ from and to arguments to its constructor) instead.
 */
 
 
+// current() returns the current element.  next() returns the next element.
+
+function logFive(sequence) {
+    var counter = 0;
+    var result = false;
+    while ((sequence.current() != null) && (counter < 5)) {
+	console.log(sequence.current());
+	//sequence.current() = sequence.current().next();
+	result = sequence.next();
+	counter++;
+    }
+
+}
+
+function ArraySeq(array) {
+    this.pos = 0;
+    this.array = array;
+}
+
+ArraySeq.prototype.next = function() {
+    if (this.pos >= this.array.length) {
+	//return null;
+	return false;
+    }
+    else {
+	this.pos++;
+	return true;
+    }
+};
+
+
+ArraySeq.prototype.current = function() {
+    if (this.pos < this.array.length) {
+	return this.array[this.pos];
+    }
+    else
+	return null;
+}
 
 
 
+logFive(new ArraySeq([1, 2]));
+//  1
+//  2
+
+//logFive(new ArraySeq([2, 3, 1, 32, 12]));
+logFive(new ArraySeq([2, 3, 1, 32, 54, 23, 12]));
+//  2, 3, 1, 32, 54      // Each number is actually on a separate line. Also, we're only printing 5,
+                         // so the 23 and the 12 do not print.
+
+logFive(new ArraySeq([]));
+//     // prints nothing
+
+logFive(new ArraySeq([456]));
+//  456
+
+
+
+
+function RangeSeq(from, to) {
+    this.pos = from;
+    this.stop = to;
+    if (from > to)
+	this.interval = -1;
+    else
+	this.interval = 1;
+}
+
+
+/*
+RangeSeq.prototype.next = function() {
+    if (this.interval > 0) {
+        if (this.pos <= this.stop) {
+	    this.pos += this.interval;
+	    return true;
+	}
+    	else
+	    return false;
+    }
+    else {
+	if (this.pos >= this.stop) {
+	    this.pos += this.interval;
+	    return true;
+	}
+	else
+	    return false;
+    }
+}
+*/
+
+
+RangeSeq.prototype.next = function() {
+    if (this.interval > 0) {
+        if (this.pos <= this.stop) {
+	    this.pos += this.interval;
+	}
+    }
+    else {
+	if (this.pos >= this.stop) {
+	    this.pos += this.interval;
+	}
+    }
+}
+
+
+RangeSeq.prototype.current = function() {
+    if (this.interval > 0) {
+	if (this.pos <= this.stop)
+	    return this.pos;
+	else
+	    return null;
+    }
+    else {
+	if (this.pos >= this.stop)
+	    return this.pos;
+	else
+	    return null;
+    }
+}
+
+
+logFive(new RangeSeq(100, 1000));
+//  100
+//  101
+//  102
+//  103
+//  104
+
+logFive(new RangeSeq(100,1));
+//  100
+//   99
+//   98
+//   97
+//   96
+
+
+
+
+
+/*
+// The book's solution to this last exercise (6.3):
+
+// I am going to use a system where a sequence object has two methods:
+//
+// * next(), which returns a boolean indicating whether there are more
+//   elements in the sequence, and moves it forward to the next
+//   element when there are.
+//
+// * current(), which returns the current element, and should only be
+//   called after next() has returned true at least once.
+
+function logFive(sequence) {
+  for (var i = 0; i < 5; i++) {
+    if (!sequence.next())
+      break;
+    console.log(sequence.current());
+  }
+}
+
+function ArraySeq(array) {
+  this.pos = -1;
+  this.array = array;
+}
+ArraySeq.prototype.next = function() {
+  if (this.pos >= this.array.length - 1)
+    return false;
+  this.pos++;
+  return true;
+};
+ArraySeq.prototype.current = function() {
+  return this.array[this.pos];
+};
+
+function RangeSeq(from, to) {
+  this.pos = from - 1;
+  this.to = to;
+}
+RangeSeq.prototype.next = function() {
+  if (this.pos >= this.to)
+    return false;
+  this.pos++;
+  return true;
+};
+RangeSeq.prototype.current = function() {
+  return this.pos;
+};
+
+logFive(new ArraySeq([1, 2]));
+//  1
+//  2
+logFive(new RangeSeq(100, 1000));
+//  100
+//  101
+//  102
+//  103
+//  104
+
+// This alternative approach represents the empty sequence as null,
+// and gives non-empty sequences two methods:
+//
+// * head() returns the element at the start of the sequence.
+// 
+// * rest() returns the rest of the sequence, or null if there are no
+//   elemements left.
+//
+// Because a JavaScript constructor can not return null, we add a make
+// function to constructors of this type of sequence, which constructs
+// a sequence, or returns null if the resulting sequence would be
+// empty.
+
+function logFive2(sequence) {
+  for (var i = 0; i < 5 && sequence != null; i++) {
+    console.log(sequence.head());
+    sequence = sequence.rest();
+  }
+}
+
+function ArraySeq2(array, offset) {
+  this.array = array;
+  this.offset = offset;
+}
+ArraySeq2.prototype.rest = function() {
+  return ArraySeq2.make(this.array, this.offset + 1);
+};
+ArraySeq2.prototype.head = function() {
+  return this.array[this.offset];
+};
+ArraySeq2.make = function(array, offset) {
+  if (offset == null) offset = 0;
+  if (offset >= array.length)
+    return null;
+  else
+    return new ArraySeq2(array, offset);
+};
+
+function RangeSeq2(from, to) {
+  this.from = from;
+  this.to = to;
+}
+RangeSeq2.prototype.rest = function() {
+  return RangeSeq2.make(this.from + 1, this.to);
+};
+RangeSeq2.prototype.head = function() {
+  return this.from;
+};
+RangeSeq2.make = function(from, to) {
+  if (from > to)
+    return null;
+  else
+    return new RangeSeq2(from, to);
+};
+
+logFive2(ArraySeq2.make([1, 2]));
+//  1
+//  2
+logFive2(RangeSeq2.make(100, 1000));
+//  100
+//  101
+//  102
+//  103
+//  104
+
+
+*/
 
---
+++ ch7.js
@@ -0,0 +1,116 @@
+// Chapter 7: Project - Electronic Life
+
+/*
+Our project in this chapter is to build a virtual ecosystem, a little world
+populated with critters that move around and struggle for survival.
+
+Definition:
+To make this task manageable, we will radically simplify the concept
+of a world. Namely, a world will be a two-dimensional grid where each
+entity takes up one full square of the grid. On every turn, the critters
+all get a chance to take some action.
+
+Thus, we chop both time and space into units with a fixed size: squares
+for space and turns for time. Of course, this is a somewhat crude and
+inaccurate approximation. But our simulation is intended to be amusing,
+not accurate, so we can freely cut such corners.
+
+We can define a world with a plan, an array of strings that lays out
+the world's grid using one character per square.
+
+The "#" characters in this plan represent walls and rocks, and the "o"
+characters represent critters. The spaces, as you might have guessed, are
+empty space.
+
+A plan array can be used to create a world object. Such an object keeps
+track of the size and content of the world. It has a toString method, which
+converts the world back to a printable string (similar to the plan it was
+based on) so that we can see what's going on inside. The world object
+also has a turn method, which allows all the critters in it to take one turn
+and updates the world to reflect their actions.
+*/
+
+var plan = [ "############################" ,
+	     "#      #    #      o      ##" ,
+	     "#                          #" ,
+	     "#          #####           #" ,
+	     "##         #   #    ##     #" ,
+	     "###           ##     #     #" ,
+	     "#           ###      #     #" ,
+	     "#   ####                   #" ,
+	     "#   ##       o             #" ,
+	     "# o  #         o       ### #" ,
+	     "#    #                     #" ,
+	     "############################" ] ;
+	     
+
+
+// Representing Space
+
+/*
+The grid that models the world has a fixed width and height. Squares
+are identified by their x- and y-coordinates. We use a simple type, Vector
+(as seen in the exercises for the previous chapter), to represent these
+coordinate pairs.
+
+ */
+
+
+function Vector(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+Vector.prototype.plus = function(other) {
+    return new Vector(this.x + other.x, this.y + other.y);
+};
+
+
+/*
+
+Next, we need an object type that models the grid itself. A grid is part
+of a world, but we are making it a separate object (which will be a
+property of a world object) to keep the world object itself simple. The
+world should concern itself with world-related things, and the grid should
+concern itself with grid-related things.
+
+When calling the Array constructor with a single number as an argument, it 
+creates a new empty array of the given length.
+
+This code defines the Grid object, with some basic methods:
+*/
+
+function Grid(width, height) {
+    this.space = new Array(width * height);
+    this.width = width;
+    this.height = height;
+}
+
+Grid.prototype.isInside = function(vector) {
+    return (vector.x >= 0 && vector.x < this.width &&
+	    vector.y >= 0 && vector.y < this.height);
+};
+
+Grid.prototype.get = function(vector) {
+    return this.space[vector.x + this.width * vector.y];
+};
+
+Grid.prototype.set = function(vector, value) {
+    this.space[vector.x + this.width * vector.y] = value;
+};
+
+// And here is a trivial test:
+
+var grid = new Grid(5,5);
+console.log(grid.get(new Vector(1,1)));    // undefined
+grid.set(new Vector(1,1), "X");
+console.log(grid.get(new Vector(1,1)));    // X
+
+
+
+
+
+
+
+
+
