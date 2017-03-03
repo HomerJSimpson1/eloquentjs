3b1df955b589e0c37f59517d9348df032012fd03
JP <vuke93@gmx.com>
2017-03-03 18:46:57 -0500

    Latest updates

--- ch8.js
+++ ch8.js
@@ -1,2 +1,34 @@
 // DEBUGGING AND ERROR HANDLING
 
+function canYouSpotTheProblem() {
+    "use strict";
+    for (counter = 0; counter < 10; counter++)
+	console.log("Happy happy");
+}
+
+canYouSpotTheProblem()  // ReferenceError: counter is not defined
+
+
+// Since we used "use strict", we need to declare counter
+// i.e. for (var counter = 0; counter < 10; counter++)
+
+
+function Person(name) {
+    this.name = name;
+}
+var Ferdinand = Person("Ferdinand");  // oops - forgot the call to new
+console.log(name);                    // Ferdinand
+
+
+// If we "use strict" here, then
+
+"use strict";
+function Person(name) {
+    this.name = name;
+}
+
+var Ferdinand = Person("Ferdinand");   // Forgot the call to new
+// TypeError: Cannot set property 'name' of undefined
+
+
+
