780d3af0585775dfe263939ae40cded0679fa9cc
JP <vuke93@gmx.com>
2017-03-17 17:55:17 -0400

    Latest updates

--- ch8.js
+++ ch8.js
@@ -32,3 +32,36 @@ var Ferdinand = Person("Ferdinand");   // Forgot the call to new
 
 
 
+
+
+// TESTING
+
+function Vector(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+Vector.prototype.plus = function(other) {
+    return new Vector(this.x + other.x, this.y + other.y);
+};
+
+
+// Now test the above function (class)
+function testVector() {
+    var p1 = new Vector(10, 20);
+    var p2 = new Vector(-10, 5);
+    var p3 = p1.plus(p2);
+
+    if (p1.x !== 10) return "fail: p1 x property";
+    if (p1.y !== 20) return "fail: p1 y property";
+    if (p2.x !== -10) return "fail: negative x property";
+    if (p2.y !== 5) return "fail: p2 y property";
+    if (p3.x !== 0) return "fail: x from plus";
+    if (p3.y !== 25) return "fail: y from plus";
+}
+
+console.log(testVector());
+// everything ok
+
+
+
